a5fb3b9bef698d72ff1a25133467a226b14c3454
Jackson <armnellgomez@gmail.com>
2025-12-16 16:28:31 -0500

    implemente Card de shadcn en ProducCard

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -2,6 +2,7 @@
 
 import type { Product} from "@/types/product"
 import Image from "next/image"
+import { Card, CardContent, CardFooter } from "@/components/ui/card"
 
 interface ProductCardProps {
   product: Product
@@ -10,22 +11,22 @@ interface ProductCardProps {
 
 export default function ProductCard({ product, onClick }: ProductCardProps) {
   return (
-    <div
-      className="flex flex-col rounded-xl overflow-hidden cursor-pointer transition-transform duration-300 hover:scale-105 hover:shadow-[0_0_15px_rgba(0,0,0,0.5)]"
+    <Card
+      className="flex flex-col rounded-xl overflow-hidden cursor-pointer transition-transform duration-300 hover:scale-105 hover:shadow-[0_0_15px_rgba(0,0,0,0.5)] border-0 p-2 bg-[#000000]"
       onClick={() => onClick(product)}
     >
-      <div className="relative h-48 w-full">
+      <CardContent className="relative h-48 w-full">
         <Image 
           src={product.image || "/e-commerce/placeholder.svg"} 
           alt={product.name} 
           fill 
           priority
-          className="object-cover" 
+          className="object-contain rounded-xl" 
         />
-      </div>
-      <div className="p-4 bg-[#000000]">
+      </CardContent>
+      <CardFooter className="p-4 bg-[#000000] flex justify-center items-center">
         <h3 className="text-[#FF0B55] font-medium text-lg truncate">{product.name}</h3>
-      </div>
-    </div>
+      </CardFooter>
+    </Card>
   )
 }
